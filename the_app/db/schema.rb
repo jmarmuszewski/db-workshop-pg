e3d688510e5cc0f7e74384f066177dd4e090bc09
Jacek Marmuszewski <jacek.marmuszewski@gmail.com>
2016-09-20 21:58:33 +0200

    Schema update

--- the_app/db/schema.rb
+++ the_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916215345) do
+ActiveRecord::Schema.define(version: 20160918192158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160916215345) do
     t.text     "reason"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_bans_on_email", using: :btree
     t.index ["user_id"], name: "index_bans_on_user_id", using: :btree
   end
 
